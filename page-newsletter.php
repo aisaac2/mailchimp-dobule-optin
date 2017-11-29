1eaced369f46325c85cf64878d644899bdd6711b
Assaf Isaac <aisaac1285@gmail.com>
2017-11-29 10:29:34 -0500

    Rename newsletter.php to page-newsletter.php

---
+++ page-newsletter.php
@@ -0,0 +1,59 @@
+<section class="section newsletter">
+  <div class="section-wrap">
+
+    <section class="section">
+    <!-- Begin MailChimp Signup Form -->
+  		<div class="cl-newsletter mc_embed_signup">
+  	    <form id="newsletterSignup" name="mc-embedded-subscribe-form" class="mc-embedded-subscribe-form validate" target="_blank" novalidate>
+          		<div class="fields">
+          			<label for="email">Email Address</label>
+          			<input id="email" type="email" value="" name="email" class="required email" id="email" placeholder="*Email">
+          		</div>
+          		<div class="fields">
+          			<label for="fname">First Name </label>
+          			<input type="text" value="" name="fname" class="" id="fname" placeholder="First Name">
+          		</div>
+          		<div class="fields">
+          			<label for="lname">Last Name </label>
+          			<input type="text" value="" name="lname" class="" id="lname" placeholder="Last Name">
+          		</div>
+          		<div class="fields mc-province">
+                <label for="province">In what province do you live? </label>
+              	<select name="province" class="REQ_CSS" id="province">
+              	<option value=""></option>
+              	<option value="2">Ontario</option>
+                <option value="4">Alberta</option>
+                <option value="8">British Columbia</option>
+                <option value="16">Manitoba</option>
+                <option value="32">New Brunswick</option>
+                <option value="64">Newfoundland &amp; Labrador</option>
+                <option value="128">Northwest Territories</option>
+                <option value="256">Nova Scotia</option>
+                <option value="512">Nunavut</option>
+                <option value="1024">Prince Edward Island</option>
+                <option value="2048">Quebec</option>
+                <option value="4096">Saskatchewan</option>
+                <option value="8192">Yukon</option>
+                <option value="16384">Outside Canada</option>
+              	</select>
+          		</div>
+              <div class="newsletter__checkbox">
+                <input type="checkbox" name="thirdparty_optin" id="thirdparty_optin" value="false">
+                <label for="thirdparty_optin">
+                  <span></span>
+                  <p class="small">I would also like to receive info and promos from trusted partners.</p>
+                </label>
+              </div>
+    			    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      		    <div style="position: absolute; left: -5000px;"><input type="text" name="b_b6f91d32382d81a8796a0c841_2a4b4abf7b" tabindex="-1" value=""></div>
+  		        <div class="clear"><input type="submit" value="Sign Up" class="cl-button"></div>
+              <div id="mce-responses" class="clear">
+          			<div class="response mce-error-response" style="display:none">Please enter a valid email address.</div>
+          			<div class="response mce-success-response" style="display:none">Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.</div>
+          		</div>
+  	    </form>
+  	  </div>
+  		<!--End mc_embed_signup-->
+    </section>
+  </div>
+</section>
